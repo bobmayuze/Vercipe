9b4cd13d3a7108c6de3594fbc1e9273dde1506de
Yuze.Ma <584653629@qq.com>
2018-02-10 16:44:22 -0500

    Finish the creating function of a recipe

--- vercipe_backend/vercipe/routes/index.js
+++ vercipe_backend/vercipe/routes/index.js
@@ -1,9 +1,31 @@
 var express = require('express');
 var router = express.Router();
+var recipeModels = require('../models/recipe');
+var MongoClient = require('mongodb').MongoClient
+var mongoose = require('mongoose');
+
+var recipe = recipeModels.recipeModel;
+
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express X' });
+});
+
+// Create a recipe
+router.post('/newRecipe', (req, res) => {
+  console.log("Creating new Recipe ")
+  var newRecipe = new recipe;
+  newRecipe.title = req.body.title || "idiot sandwich";
+  newRecipe.creater = req.body.creater || "Gordon Ramsay";
+  newRecipe.save((err) => {
+    if (err) console.log(err);
+    else {
+      console.log("successfully registered");
+      res.send("success ... ");
+    }
+  });
 });
 
 module.exports = router;
