141f7ea60da61062058d6ef72767b8736e9b5da0
Yuze.Ma <584653629@qq.com>
2018-02-13 00:07:31 -0500

    Fix issue of using ENTERPOINT

--- vercipe_backend/vercipe/routes/users.js
+++ vercipe_backend/vercipe/routes/users.js
@@ -11,22 +11,24 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+// Sign up
 router.post('/sign_up', function (req, res, next) {
   service.createUser(req,res);
   // res.send('respond with a resource');
 });
 
+// Login, this will attach session to user
 router.post('/log_in', (req,res,next)=>{
   console.log("LOG IN");
   service.login(req, res);
 });
 
+// dashborad for checking if used is loged in or not
 router.get('/dashboard', (req, res, next) => {
   var login = service.checkStatus(req, res);
   if (login) {
     console.log("Welcome to Vercipeeeee");
     res.send("Welcome to Vercipeeeee");
-
   }
   else {
     console.log("Please login first");
