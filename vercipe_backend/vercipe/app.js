d170cbbf8f00e901c151dd886410cc5245749087
Yuze.Ma <584653629@qq.com>
2018-03-02 14:36:35 -0500

    Allow cross origin request

--- vercipe_backend/vercipe/app.js
+++ vercipe_backend/vercipe/app.js
@@ -28,6 +28,14 @@ app.use(session({
   saveUninitialized: true
 }));
 
+// CROS request handle
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+
 app.use('/', index);
 app.use('/users', users);
 
