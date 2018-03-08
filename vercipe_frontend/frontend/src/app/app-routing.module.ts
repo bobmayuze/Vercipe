a23527bbd616c16ab9f3999c26202dea9a456c8f
carlsc3 <h.warrior14@gmail.com>
2018-03-07 21:12:50 -0500

    AND some routing I added to test/play with the pages

--- vercipe_frontend/frontend/src/app/app-routing.module.ts
+++ vercipe_frontend/frontend/src/app/app-routing.module.ts
@@ -4,13 +4,18 @@ import { CommonModule } from '@angular/common';
 import { SearchBoxComponent } from './search-box/search-box.component';
 import { CreateBoardComponent } from './create-board/create-board.component';
 import { DetailBoardComponent } from './detail-board/detail-board.component';
+import { LogInComponent } from './user/log-in/log-in.component';
+import { SignUpComponent } from './user/sign-up/sign-up.component';
 
 const routes: Routes = [
   // Default route
   { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
   { path: 'dashboard', component: SearchBoxComponent },
   { path: 'create', component: CreateBoardComponent },
-  { path: 'detail', component: DetailBoardComponent }
+  { path: 'detail', component: DetailBoardComponent },
+  { path: 'login', component: LogInComponent },
+  { path: 'signup', component: SignUpComponent }
+  
   // { path: 'search', component: HeroSearchComponent },
   // { path: 'detail/:id', component: HeroDetailComponent },
   // // It routes to heros
