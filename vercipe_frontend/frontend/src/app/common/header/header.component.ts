51901ce8df41da6677b23c6be801b2ae2fac827b
Yuze.Ma <584653629@qq.com>
2018-03-17 16:48:30 -0400

    increase UX of login function

--- vercipe_frontend/frontend/src/app/common/header/header.component.ts
+++ vercipe_frontend/frontend/src/app/common/header/header.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { userInfo } from 'os';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +9,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
+  }
+
+  login() {
+    console.log(`Clicked`);
+    const loginFlag: any = JSON.stringify(sessionStorage.getItem(`currentUser`));
+    console.log(loginFlag);
+    if (loginFlag === `null`) {
+      console.log(`Please Login First`);
+      this.router.navigateByUrl('/login');
+    } else {
+      console.log(`User logged in already`);
+      this.router.navigateByUrl('/dashboard');
+    }
   }
 
 }
