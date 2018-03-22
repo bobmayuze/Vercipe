ea800b69b18c508a6746cebaa778160e70351c64
carlsc3 <h.warrior14@gmail.com>
2018-03-22 16:21:43 -0400

    Initial Design of User Dashboard

--- vercipe_frontend/frontend/src/app/app-routing.module.ts
+++ vercipe_frontend/frontend/src/app/app-routing.module.ts
@@ -7,7 +7,8 @@ import { DetailBoardComponent } from './detail-board/detail-board.component';
 import { LogInComponent } from './user/log-in/log-in.component';
 import { SignUpComponent } from './user/sign-up/sign-up.component';
 import { RecipeListComponent } from './recipe-list/recipe-list.component';
-import { PageNotFoundComponent } from './common/page-not-found/page-not-found.component';
+import { DashboardComponent } from './user/dashboard/dashboard.component';
+
 
 const routes: Routes = [
   // Default route
@@ -17,10 +18,12 @@ const routes: Routes = [
   { path: 'detail', component: DetailBoardComponent },
   { path: 'login', component: LogInComponent },
   { path: 'signup', component: SignUpComponent },
-  { path: 'recipes', component: RecipeListComponent },
-  { path: 'recipes/:title', component: RecipeListComponent },
-  { path: '404', component: PageNotFoundComponent },
-  // { path: '/*path', redirectTo: ['NotFound'] }
+  { path: 'all', component: RecipeListComponent },
+  { path: 'user', component: DashboardComponent }
+  // { path: 'search', component: HeroSearchComponent },
+  // { path: 'detail/:id', component: HeroDetailComponent },
+  // // It routes to heros
+  // { path: 'heroes', component: HeroesComponent }
 ];
 
 @NgModule({
